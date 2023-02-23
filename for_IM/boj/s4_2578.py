a62507d3dd4ec56ddab5d4fcf9f387cdf2e91bbc
Andrea <wjdgnstjr45@gmail.com>
2023-02-23 15:48:33 +0900

    20230223

--- for_IM/boj/s4_2578.py
+++ for_IM/boj/s4_2578.py
@@ -1,55 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-arr = [list(map(int, input().split())) for _ in range(5)]
+arr1 = [list(map(int,input().split()))for i in range(5)]
+arr2 = list(map(list,zip(*arr1)))
 
-s = []
-for _ in range(5):
-    s += list(map(int, input().split()))
+arr3 = []
+arr4 = []
+for i in range(len(arr1)) :
+    arr3.append(arr1[i][i]) 
+    arr4.append(arr2[i][4-i]) 
+arr = arr1+arr2
+arr.append(arr3)
+arr.append(arr4) 
 
-cnt = 0
-result = 0
-for n in s:
-    check = True
-    for i in range(5):
-        for j in range(5):
-            if arr[i][j] == n:
-                arr[i][j] = 0
-                cnt += 1
-                check = False
-                break
-        if not check:
-            break
-    if cnt >= 12:
-        x1_cnt = 0
-        x2_cnt = 0
-        for i in range(5):
-            row_cnt = 0
-            col_cnt = 0
-            for j in range(5):
-                if arr[i][j] == 0:
-                    row_cnt += 1
-                if arr[j][i] == 0:
-                    col_cnt += 1
-            if i == 0:
-                if arr[i][i] == 0:
-                    x1_cnt += 1
-                if arr[i][-i-1] == 0:
-                    x2_cnt += 1
-            if row_cnt == 5:
-                result += 1
-                print('row')
-            if col_cnt == 5:
-                result += 1
-                print('col')
-        if x1_cnt == 5:
-            result += 1
-            print('x1')
-        if x2_cnt == 5:
-            result += 1
-            print('x2')
-        if result >= 3:
-            for k in arr:
-                print(*k)
-            print(cnt)
-            exit()
+lst = []
+for i in range(5) :
+    lst += list(map(int,input().split())) 
+n = 0
+for i in lst :
+    n +=1
+    cnt = 0
+    for j in arr :
+        if i in j :
+            j.remove(i) 
+        if j == [] : 
+            cnt +=1
+    if cnt >= 3: 
+        break
+print(n) 
