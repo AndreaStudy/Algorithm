f84c1355bb5f376944968aadbff064aad4f033d4
Andrea <wjdgnstjr45@gmail.com>
2023-02-27 17:02:42 +0900

    20230227

---
+++ queue/g3_2206.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+from collections import deque  
+
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+N, M = map(int,input().split())
+maze = [list(input().rstrip()) for _ in range(N)]
+visited = [[[0, 0] for _ in range(M)] for _ in range(N)]
+
+def bfs():
+
+    q = deque()
+    q.append((0,0,0))
+    visited[0][0][0] = 1
+
+    while q:
+
+        x,y,broken = q.popleft()
+
+        if x == N-1 and y == M-1:
+            return visited[x][y][broken]
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny][broken] == 0:
+                if maze[nx][ny] == '0':
+                    q.append((nx,ny,broken))
+                    visited[nx][ny][broken] = visited[x][y][broken] + 1
+                if broken == 0 and maze[nx][ny] == '1':
+                    q.append((nx,ny,1))
+                    visited[nx][ny][1] = visited[x][y][broken] + 1
+    return -1
+
+print(bfs())
+
+
+
+
+            
--- queue/s2_5464.py
+++ queue/s2_5464.py
@@ -9,30 +9,29 @@ seq = [int(input()) for _ in range(2*M)]
 visited = [0] * N
 result = 0
 q = deque()
+push = True
 for i in seq:
-    if 0 in visited:
-        if i > 0:
-            for j in range(N):
-                if visited[j] == 0:
-                    result += (cost[j]*cars[i-1])
-                    visited[j] = i
-                    break
-        else:
-            i = abs(i)
-            for j in range(N):
-                if visited[j] == i:
-                    visited[j] = 0
-                    if q:
-                        temp = q.popleft()
-                        result += (cost[j]*cars[temp-1])
-                        visited[j] = temp
-                        print('wow')
-                    break
-                
+    if i > 0:
+        for j in range(N):
+            if visited[j] == 0:
+                result += (cost[j]*cars[i-1])
+                visited[j] = i
+                push = True
+                break
+            else:
+                push = False
+        if not push:
+            q.append(i)
     else:
-        print(i, *visited)
-        print(1111)
-        q.append(i)
+        i = abs(i)
+        for j in range(N):
+            if visited[j] == i:
+                visited[j] = 0
+                if q:
+                    temp = q.popleft()
+                    result += (cost[j]*cars[temp-1])
+                    visited[j] = temp
+                break
 
 print(result)
 
