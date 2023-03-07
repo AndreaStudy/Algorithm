013ac93cc490ffc1ac11b5687c92065bea367949
Andrea <wjdgnstjr45@gmail.com>
2023-03-07 21:04:40 +0900

    20230307

--- dp/s1_1149.py
+++ dp/s1_1149.py
@@ -1,30 +1,12 @@
 import sys
-sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
-
-def find(n, m):
-    global result, total
-    if n != 0:
-        visited[m] = 1
-    if n == N:
-        print(result, total)
-        if result > total:
-            result = total
-        return
-    for i in range(3):
-        if visited[i] == 0:
-            total += arr[n][i]
-            find(n+1, i)
-            total -= arr[n][i]
-        else:
-            continue
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-visited = [0]*3
 
-result = 1000*N
-total = 0
+for i in range(N - 1):
+    arr[i + 1][0] = min(arr[i][1], arr[i][2]) + arr[i + 1][0]
+    arr[i + 1][1] = min(arr[i][0], arr[i][2]) + arr[i + 1][1]
+    arr[i + 1][2] = min(arr[i][0], arr[i][1]) + arr[i + 1][2]
 
-find(0, 0)
-print(result)
+print(min(arr[N-1]))
