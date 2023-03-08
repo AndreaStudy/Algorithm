240a3e9c716fd1617600a851388bf8459152e00b
Andrea <wjdgnstjr45@gmail.com>
2023-03-08 23:51:22 +0900

    20230308

--- DFS/g4_9663.py
+++ DFS/g4_9663.py
@@ -1,23 +1,6 @@
 import sys
 sys.setrecursionlimit(10**9)
 
-dx = [-1, 0, 1]
-dy = [-1, -1, -1]
-
-def check(a, b):
-    for d in range(3):
-        i = 1
-        while True:
-            ny = a + i*dy[d]
-            nx = b + i*dx[d]
-            if 0 <= ny < N and 0 <= nx < N:
-                if arr[ny][nx] == 1:
-                    return False
-            else:
-                break
-            i += 1
-    return True
-
 
 def dfs(n):
     global cnt
@@ -25,20 +8,19 @@ def dfs(n):
         cnt += 1
         return
     for d in range(N):
-        if visited[d] == 0:
-            visited[d] = 1
-            arr[n][d] = 1
-            if check(n, d):
-                dfs(n+1)
-            visited[d] = 0
-            arr[n][d] = 0
+        if not (visited1[d] or visited2[n+d] or visited3[n-d]):
+            visited1[d] = visited2[n+d] = visited3[n-d] = 1
+            dfs(n+1)
+            visited1[d] = visited2[n+d] = visited3[n-d] = 0
+
 
 N = int(input())
 
-visited = [0]*N
-arr = [[0]*N for _ in range(N)]
+visited1 = [0]*N
+visited2 = [0]*(2*N)
+visited3 = [0]*(2*N)
+
 cnt = 0
 dfs(0)
 
 print(cnt)
-
--- exam/g5_1759.py
+++ exam/g5_1759.py
@@ -2,32 +2,32 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
-def dfs(n, cnt, check):
-    global result
-    if cnt == 4 and check:
-        print(result)
-        return
-    for i in range(n, C):
-        if visited[i] == 0:
-            temp = result
-            result = result + arr[i]
-            visited[i] = 1
-            if arr[i] in idx:
-                dfs(i, cnt+1, True)
+
+def dfs(cnt, idx):
+
+    if cnt == L:
+        moeum, jaeum = 0, 0
+
+        for i in range(L):
+            if answer[i] in 'aeiou':
+                moeum += 1
             else:
-                dfs(i, cnt+1, check)
-            result = temp
-            visited[i] = 0
+                jaeum += 1
+
+        if moeum >= 1 and jaeum >= 2:
+            print("".join(answer))
+
+        return
+
+    for i in range(idx, C):
+        answer.append(arr[i])
+        dfs(cnt + 1, i + 1)
+        answer.pop()
 
 
 L, C = map(int, input().split())
 arr = list(input().split())
 arr.sort()
-idx = []
-visited = [0] * C
-for i in range(C):
-    if arr[i] in 'aeiou':
-        idx.append(arr[i])
 
-result = ''
-dfs(0, 0, False)
+answer = []
+dfs(0, 0)
